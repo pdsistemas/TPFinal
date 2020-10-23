9e4dcf79ead4215cb5879f6a1b3b3bca8f759060
pdsistemas <pdsistemas88@gmail.com>
2020-10-23 13:52:10 -0300

    se agrega str

--- clienteCorporativo.py
+++ clienteCorporativo.py
@@ -2,7 +2,7 @@
 from cliente import Cliente
 
 class ClienteCorporativo(Cliente):
-    '''Representa un cliente particular'''
+    '''Representa un cliente corporativo'''
     def __init__(self, nombre_empresa, nombre_contacto, telefono_contacto,
             telefono, mail, id_cliente = None):
         self.nombre_empresa = nombre_empresa
@@ -10,3 +10,8 @@ class ClienteCorporativo(Cliente):
         self.telefono_contacto = telefono_contacto
         super().__init__(telefono, mail, id_cliente)
          
+    def __str__(self):
+        cadena = f"{self.id_cliente}: {self.nombre_empresa}\nTipo: Cliente corporativo\n"
+        cadena+= f"Tel/mail: {self.telefono} - {self.mail}\n"
+        cadena+= f"Contacto (Tel/mail): {self.nombre_contacto} {self.telefono_contacto}"
+        return cadena
