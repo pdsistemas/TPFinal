e37ef806edc280f6ff1a5cb8f9cfc3fde750a1f6
pdsistemas <pdsistemas88@gmail.com>
2020-10-23 13:43:57 -0300

    Se corrigen errores en listatrabajo

--- Listatrabajo.py
+++ Listatrabajo.py
@@ -5,12 +5,23 @@ import datetime
 
 class TrabajosLista:
 
+
     def __init__(self):
         self.rt = RepositorioTrabajos()
         self.lista_trabajos = self.rt.get_all()
 
     def nuevo_trabajo(self, cliente, fecha_ingreso, fecha_entrega_propuesta, descripcion):
-        """Crea un nuevo trabajo y lo agrega a la lista y a la base de datos"""
+        """Crea un nuev trabajo y lo agrega a la lista y a la base de datos"""
+        t = Trabajo(cliente, fecha_ingreso, fecha_entrega_propuesta, None, descripcion, False)
+        t.id_trabajo = self.rt.store(t)
+        if t.id_trabajo == 0:
+            return None
+        else:
+            self.lista_trabajos.append(t)
+            return t
+
+    def borrar_trabajo(self, cliente, fecha_ingreso, fecha_entrega_propuesta, descripcion):
+        """Crea un nuev trabajo y lo agrega a la lista y a la base de datos"""
         t = Trabajo(cliente, fecha_ingreso, fecha_entrega_propuesta, None, descripcion, False)
         t.id_trabajo = self.rt.store(t)
         if t.id_trabajo == 0:
